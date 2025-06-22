7593dcbf40d528ed7b60dbd003e0115e0983793b
sunmin <ekarha1209@gmail.com>
2025-06-22 15:09:18 +0900

    스터디기록, 스터디덧글 엔티티 추가

--- src/main/java/com/example/studymate/Study/entity/StudyComment.java
+++ src/main/java/com/example/studymate/Study/entity/StudyComment.java
@@ -1,6 +1,9 @@
 package com.example.studymate.Study.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
@@ -10,14 +13,14 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-public class StudyMember {
-
+public class StudyComment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long userId;
-    private Long studyGroupId;
+    private Long recordId;
+    private Long authorId;
+    private String authorName;
+    private String content;
     @CreatedDate
     private LocalDateTime createdAt;
-
 }
--- src/main/java/com/example/studymate/Study/entity/StudyRecord.java
+++ src/main/java/com/example/studymate/Study/entity/StudyRecord.java
@@ -1,6 +1,9 @@
 package com.example.studymate.Study.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
@@ -10,13 +13,16 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-public class StudyMember {
+public class StudyRecord {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long userId;
     private Long studyGroupId;
+    private Long authorId;
+    private Long authorName;
+    private String title;
+    private String content;
     @CreatedDate
     private LocalDateTime createdAt;
 
